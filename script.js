c271192880110f06496308b485f160032b8b6240
Ivanandrade887 <043la21@upch.mx>
2024-11-27 09:03:29 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ function redirigirAInicio(event) {
     event.preventDefault(); // Evita el envío del formulario
     window.location.href = "index.html"; // Redirige a index.html
   }
+
+  function mostrarAlerta() {
+    alert("¡Nuevas funciones muy pronto!");
+}
